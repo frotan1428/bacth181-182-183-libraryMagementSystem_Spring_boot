52200e4518dd4f1e4bac30a4218d90e45cbacaff
MFROTAN <98325864+frotan1428@users.noreply.github.com>
2023-10-14 23:09:57 +0430

    LMS-1

---
+++ src/main/java/com/tpe/domain/Book.java
@@ -0,0 +1,41 @@
+package com.tpe.domain;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name ="tbl_book")
+
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @NotNull(message = "title Can Not Be null")
+    @NotBlank(message = "title can not be White Space..")
+    @Size(min = 4,max = 25 ,message = "title '${validatedValue}' must be between : {min} and {max}  ...")//2
+    private String title;
+
+    private String author;
+
+    @Column(nullable = false)
+    private String publicationDate;
+
+    @ManyToMany(mappedBy = "books",cascade = CascadeType.REMOVE,fetch = FetchType.LAZY)
+    private List<Teacher> teachers = new ArrayList<>();
+
+
+}
---
+++ src/main/java/com/tpe/domain/Teacher.java
@@ -0,0 +1,59 @@
+package com.tpe.domain;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tbl_teacher")
+
+public class Teacher {//teacher
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @NotNull(message = "Name Can Not Be null")
+    @NotBlank(message = "Name can not be White Space..")
+    @Size(min = 4,max = 25 ,message = "Name '${validatedValue}' must be between : {min} and {max}  ...")//2
+    private String  name;
+
+
+    @Column(name = "last_name",nullable = false)
+    private String lastName;
+
+    @Email(message = "Please provide a valid email...")
+    @Column(nullable = false ,length = 30,unique = true)
+    private String email;
+
+    private String phoneNumber;
+
+
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime registerDate;
+
+
+    @ManyToMany
+    @JoinTable(name = "teacher_book",
+   joinColumns = @JoinColumn(name = "teacher_id"),
+    inverseJoinColumns = @JoinColumn(name = "book_id"))
+    private List<Book> books= new ArrayList<>();
+
+
+
+
+}
